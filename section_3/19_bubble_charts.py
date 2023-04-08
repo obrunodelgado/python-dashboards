7fca85e8ea676ad51efd12dee0cef48614460e42
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-08 15:58:23 +0100

    S3E19 - Bubble Chart

---
+++ section_3/19_bubble_charts.py
@@ -0,0 +1,28 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('../data/mpg.csv')
+
+data = [go.Scatter(
+    x=df['horsepower'],
+    y=df['mpg'],
+    text=df['name'],
+    mode='markers',
+    marker=dict(
+        size=df['weight']/100,
+        color=df['cylinders'],
+        showscale=True
+    )
+)]
+
+layout = go.Layout(
+    title='Bubble Chart',
+    xaxis=dict(title='Horsepower'),
+    yaxis=dict(title='MPG'),
+    hovermode='closest'
+)
+
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig, filename='../charts/19_bubble_charts.html')
