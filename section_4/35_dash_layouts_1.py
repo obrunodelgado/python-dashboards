f4d0390aa545a0cadff57c0a3985c85e5cc73f63
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-08 18:45:42 +0100

    S04E35 - First Dash Layout

--- section_4/35_dash_layouts_1.py
+++ section_4/35_dash_layouts_1.py
@@ -7,6 +7,19 @@ app = dash.Dash()
 app.layout = html.Div(
     children=[
         html.H1('Dash Tutorials'),
+        html.Div('Dash: Web Dashboards with Python & React'),
+        dcc.Graph(
+            id='example',
+            figure={
+                'data': [
+                    {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                    {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+                ],
+                'layout': {
+                    'title': 'Dash Data Visualization'
+                }
+            }
+        )
     ]
 )
 
