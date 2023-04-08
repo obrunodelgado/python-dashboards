15389971a8d3c879dc3d3a287851b39dd7dc739e
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-08 16:12:52 +0100

    S03E22 - Box Plot

---
+++ section_3/22_box_plots_2.py
@@ -0,0 +1,15 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+y = [1,14,14,15,16,18,18,19,19,20,20,23,24,26,27,27,28,29,33,54]
+
+data = [
+    go.Box(
+        y=y,
+        boxpoints='all', # display the original data points
+        jitter=0.3,      # spread them out so they all appear
+        pointpos=-1.8    # offset them to the left of the box
+    )
+]
+
+pyo.plot(data, filename='../charts/22_box_plots_2.html')
