353fdf3b6d40d50ea8ccba4c6cc261285f4ba2f7
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-08 14:45:55 +0100

    S3C16 - Bar Charts

---
+++ section_3/16_bar_charts_1.py
@@ -0,0 +1,18 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('../data/2018WinterOlympics.csv')
+
+data = [go.Bar(
+    x=df['NOC'],
+    y=df['Total']
+)]
+
+layout = go.Layout(
+    title='2018 Winter Olympics Medals by Country'
+)
+
+fig = go.Figure(data=data, layout=layout)
+
+pyo.plot(fig, filename='16_bar_charts.html')
