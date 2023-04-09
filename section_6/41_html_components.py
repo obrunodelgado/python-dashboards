e3a971721a7b3b72becde78b9838c266878d4d58
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-09 08:04:19 +0100

    S06E41 - Html Components

---
+++ section_6/41_html_components.py
@@ -0,0 +1,31 @@
+import dash
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    [
+        'This is the outermost div!',
+        html.Div(
+            'This is an inner div!',
+            style={
+                'color': 'red',
+                'border': '2px red solid'
+            }
+        ),
+        html.Div(
+            'This is another inner div!',
+            style={
+                'color': 'green',
+                'border': '2px green solid'
+            }
+        )
+    ],
+    style={
+      'color': 'blue',
+      'border': '2px blue solid'
+    }
+)
+
+if __name__ == '__main__':
+    app.run_server()
