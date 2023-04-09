0714b6ec8499524fa9993ace8c9da4dceb78b364
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-09 12:39:20 +0100

    S07E46: Dash callbacks for graphs

---
+++ section_7/46_dash_callbacks_for_graphs.py
@@ -0,0 +1,55 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('../data/gapminderDataFiveYear.csv')
+
+app = dash.Dash()
+
+year_options = []
+for year in df['year'].unique():
+    year_options.append({'label':str(year), 'value':year})
+
+app.layout = html.Div([
+    dcc.Graph(id='graph'),
+    dcc.Dropdown(id='year-picker', options=year_options, value=df['year'].min())
+])
+
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('year-picker', 'value')]
+)
+def update_figure(selected_year):
+    filtered_df = df[df['year'] == selected_year]
+
+    traces = []
+
+    for continent_name in filtered_df['continent'].unique():
+        df_by_continent = filtered_df[filtered_df['continent'] == continent_name]
+        traces.append(go.Scatter(
+            x=df_by_continent['gdpPercap'],
+            y=df_by_continent['lifeExp'],
+            mode='markers',
+            opacity=0.7,
+            marker={'size':15},
+            name=continent_name
+        ))
+
+    return {
+        'data': traces,
+        'layout': go.Layout(
+            xaxis={'title': 'GDP Per Capita', 'type': 'log'},
+            yaxis={'title': 'Life Expectancy'},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            legend={'x': 0, 'y': 1},
+            hovermode='closest'
+        )
+    }
+
+
+if __name__ == '__main__':
+    app.run_server()
