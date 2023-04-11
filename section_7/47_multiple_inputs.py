c6badf9276c9e31c2d4fd85c11773e0b8142e767
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-11 09:50:47 +0100

    S07E47 - Multiple Inputs

---
+++ section_7/47_multiple_inputs.py
@@ -0,0 +1,69 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+
+df = pd.read_csv('../data/mpg.csv')
+
+app = dash.Dash()
+
+features = df.columns
+
+app.layout = html.Div([
+    html.Div(
+        [
+            dcc.Dropdown(
+                id='xaxis',
+                options=[{'label': i.title(), 'value': i} for i in features],
+                value='displacement'
+            )
+        ],
+        style={'width': '48%', 'display': 'inline-block'}
+    ),
+    html.Div(
+        [
+            dcc.Dropdown(
+                id='yaxis',
+                options=[{'label': i.title(), 'value': i} for i in features],
+                value='mpg'
+            )
+        ],
+        style={'width': '48%', 'float': 'right', 'display': 'inline-block'}
+    ),
+    dcc.Graph(id='feature-graphic')
+], style={'padding': 10})
+
+
+@app.callback(
+    Output('feature-graphic', 'figure'),
+    [
+        Input('xaxis', 'value'),
+        Input('yaxis', 'value')
+    ]
+)
+def update_graph(xaxis_name, yaxis_name):
+    return {
+        'data': [go.Scatter(
+            x=df[xaxis_name],
+            y=df[yaxis_name],
+            text=df['name'],
+            mode='markers',
+            marker={
+                'size': 15,
+                'opacity': 0.5,
+                'line': {'width': 0.5, 'color': 'white'}
+            }
+        )],
+        'layout': go.Layout(
+            xaxis={'title': xaxis_name.title()},
+            yaxis={'title': yaxis_name.title()},
+            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
+            hovermode='closest'
+        )
+    }
+
+
+if __name__ == '__main__':
+    app.run_server()
