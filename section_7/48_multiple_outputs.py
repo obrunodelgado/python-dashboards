cff65104626fa096ddf3e8ea86db7c0907e1b778
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-12 09:04:54 +0100

    S07E48 - Multiple Outputs

---
+++ section_7/48_multiple_outputs.py
@@ -0,0 +1,72 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+import plotly.graph_objs as go
+import pandas as pd
+import base64
+
+df = pd.read_csv('../data/wheels.csv')
+
+app = dash.Dash()
+
+
+def encode_image(image_file):
+    encoded = base64.b64encode(open(image_file, 'rb').read())
+    return f'data:image/png;base64,{encoded.decode()}'
+
+
+app.layout = html.Div(
+    [
+        dcc.RadioItems(
+            id='wheels',
+            options=[{'label': i, 'value': i} for i in df['wheels'].unique()],
+            value=1
+        ),
+        html.Div(id='wheels-output'),
+        html.Hr(),
+        dcc.RadioItems(
+            id='colors',
+            options=[{'label': i, 'value': i} for i in df['color'].unique()],
+            value='blue'
+        ),
+        html.Div(id='colors-output'),
+        html.Img(id='display-image', src='children', height=300)
+    ],
+    style={
+        'fontFamily': 'helvetica',
+        'fontSize': 18
+    }
+)
+
+
+@app.callback(
+    Output('wheels-output', 'children'),
+    [Input('wheels', 'value')]
+)
+def callback_a(wheels_value):
+    return f'You\'ve selected "{wheels_value}"'
+
+
+@app.callback(
+    Output('colors-output', 'children'),
+    [Input('colors', 'value')]
+)
+def callback_b(colors_value):
+    return f'You\'ve selected "{colors_value}"'
+
+
+@app.callback(
+    Output('display-image', 'src'),
+    [
+        Input('wheels', 'value'),
+        Input('colors', 'value')
+    ]
+)
+def callback_image(wheel, color):
+    path = '../data/images/'
+    return encode_image(path + df[(df['wheels'] == wheel) & (df['color'] == color)]['image'].values[0])
+
+
+if __name__ == '__main__':
+    app.run_server()
