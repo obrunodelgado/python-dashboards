8749a4c982bfbd1a3374451452445c74a76e2dca
Bruno Delgado <bruno.delgado@gympass.com>
2023-04-18 09:14:17 +0100

    S10E60 - Milestone project part two reading data with pandas datareader

---
+++ section_10/60_milestone_project_part_two_reading_data_with_pandas_datareader.py
@@ -0,0 +1,40 @@
+import dash
+from dash import dcc
+from dash import html
+from dash.dependencies import Input, Output
+
+app = dash.Dash()
+
+app.layout = html.Div([
+    html.H1('Stock Ticker Dashboard'),
+    html.H3('Enter a stock symbol:'),
+    dcc.Input(id='my_stock_picker', value='TSLA'),
+    dcc.Graph(
+        id='my_graph',
+        figure={
+            'data':
+                [
+                    {'x': [1, 2], 'y': [3, 1]}
+                ],
+            'layout': {'title': 'Default Title'}
+        }
+    )
+])
+
+
+@app.callback(
+    Output('my_graph', 'figure'),
+    [Input('my_stock_picker', 'value')]
+)
+def update_graph(stock_ticker):
+    fig = {
+        'data': [
+            {'x': [1, 2], 'y': [3, 1]}
+        ],
+        'layout': {'title': stock_ticker}
+    }
+    return fig
+
+
+if __name__ == '__main__':
+    app.run_server()
